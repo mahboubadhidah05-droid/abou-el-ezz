5b7a3979a82e58bcbc3017eb7aa199c8fc659320
mahboubadhidah05-droid <mahbouba.dhidah05@gmail.com>
2026-03-25 15:57:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btn").addEventListener("click", function () {
+        document.getElementById("msg").innerText = "مدة و تتعدى";
+    });
+});
